b110b0cc08380bed1a5faf0897540b1b71c33dd6
Igor Fortunato <igor-fortunatonep1@hotmail.com>
2025-04-03 21:18:51 -0300

    feat: leitura do arquivo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-	return 0;
+	string nomeArquivo;
+	
+	cout << "Digite o nome do arquivo: ";
+	cin >> nomeArquivo;
+	while (nomeArquivo != "")
+	{
+		ifstream entrada("./instancias/" + nomeArquivo);
+		if(!entrada.is_open()){
+			cout << "Erro ao abrir o arquivo de instancia." << endl;
+			return 1;
+		}
+				
+		entrada.close();
+		
+		cout << "Digite o nome do arquivo: ";
+		cin >> nomeArquivo;
+	}
 }

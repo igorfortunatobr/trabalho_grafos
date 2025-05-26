b1288d8298c7753c4bb2ff0b55d862b43ff483eb
Igor Fortunato <igor-fortunatonep1@hotmail.com>
2025-05-25 23:30:37 -0300

    Revert "Update colonia_formigas.cpp"
    This reverts commit 3ec2882a271346e0bf8d2b29f993ec0a25a40c89.

--- colonia_formigas.cpp
+++ colonia_formigas.cpp
@@ -7,8 +7,6 @@
 #include <fstream>
 #include <chrono>
 #include <iostream>
-#include <map>
-
 using namespace std;
 
 const double INFLUENCIA_FEROMONIO = 1.0; // Influência do feromônio
@@ -47,13 +45,12 @@ double heuristica(int distancia) {
 }
 
 // Escolhe o próximo serviço baseado em probabilidade
-int escolherProximo(const Servico& atual, const vector<Servico>& candidatos, const map<pair<int, int>, double>& feromonio, const vector<vector<int>>& dist) {
+int escolherProximo(const Servico& atual, const vector<Servico>& candidatos, const vector<vector<double>>& feromonio, const vector<vector<int>>& dist) {
     vector<double> vdProbabilidade;
     double soma = 0;
 
     for (const Servico& s : candidatos) {
-        auto chave = make_pair(atual.iVertice2, s.iVertice1);
-		double f = pow(feromonio.at(chave), INFLUENCIA_FEROMONIO);
+        double f = pow(feromonio[atual.iVertice1][s.iVertice1], INFLUENCIA_FEROMONIO);
         double h = pow(heuristica(dist[atual.iVertice1][s.iVertice1]), INFLUENCIA_HEURISTICA);
         vdProbabilidade.push_back(f * h);
         soma += vdProbabilidade.back();
@@ -98,7 +95,7 @@ vector<Servico> extrairServicos(const Grafo& grafo) {
 }
 
 // Construção de solução por uma formiga
-Solucao construirSolucao(const Grafo& grafo, const vector<vector<int>>& distancias, map<pair<int, int>, double>& feromonio) {
+Solucao construirSolucao(const Grafo& grafo, const vector<vector<int>>& distancias, vector<vector<double>>& feromonio) {
     vector<Servico> servicos = extrairServicos(grafo);
     Solucao solucao;
 
@@ -118,13 +115,6 @@ Solucao construirSolucao(const Grafo& grafo, const vector<vector<int>>& distanci
             Servico servicoFicticioBase = {0, atual, atual, 0, 0, true};
             int idEscolhido = escolherProximo(servicoFicticioBase, candidatos, feromonio, distancias);
             Servico servicoEscolhido = candidatos[idEscolhido];
-            
-			for (auto& servico : servicos) {
-				if (servico.id == servicoEscolhido.id) {
-					servico.atendido = true;
-					break;
-				}
-			}
 
             rota.custoTotal += distancias[atual][servicoEscolhido.iVertice1] + servicoEscolhido.custo;
             rota.demandaTotal += servicoEscolhido.demanda;
@@ -151,11 +141,7 @@ Solucao construirSolucao(const Grafo& grafo, const vector<vector<int>>& distanci
 // Função principal do ACO
 Solucao executarACO(const Grafo& grafo, const vector<vector<int>>& dist) {
     int iNumeroVertices = grafo.numVertices + 1;
-    map<pair<int, int>, double> feromonio;
-    
-	for (int i = 0; i < iNumeroVertices; ++i)
-		for (int j = 0; j < iNumeroVertices; ++j)
-			feromonio[{i, j}] = 1.0;
+    vector<vector<double>> feromonio(iNumeroVertices, vector<double>(iNumeroVertices, 1.0));
 
     Solucao melhorSolucao;
     melhorSolucao.iCustoTotal = INF;
@@ -172,18 +158,19 @@ Solucao executarACO(const Grafo& grafo, const vector<vector<int>>& dist) {
         }
 
         // Evaporação
-        for (auto& par : feromonio)
-			par.second *= (1.0 - TAXA_EVAPORACAO_FEROMONIO);
+        for (int i = 0; i < iNumeroVertices; ++i)
+            for (int j = 0; j < iNumeroVertices; ++j)
+                feromonio[i][j] *= (1 - TAXA_EVAPORACAO_FEROMONIO);
 
         // Atualiza feromônio com base nas soluções
         for (const auto& solucao : vsPopulacaoSolucoes) {
             for (const auto& rota : solucao.rotas) {
                 int atual = grafo.deposito;
                 for (const auto& servico : rota.vsServicos) {
-                    feromonio[{atual, servico.iVertice1}] += 1.0 / solucao.iCustoTotal;
+                    feromonio[atual][servico.iVertice1] += 1.0 / solucao.iCustoTotal;
                     atual = servico.iVertice2;
                 }
-                feromonio[{atual, grafo.deposito}] += 1.0 / solucao.iCustoTotal;
+                feromonio[atual][grafo.deposito] += 1.0 / solucao.iCustoTotal;
             }
         }
     }
